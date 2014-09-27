9a2c6b926edc6c913d8a6ed2ba80f1cf2652df95
moloradio <aldo@mbp-de-aldo.gateway.2wire.net>
2014-09-27 02:11:12 -0500

    cambio1
    inicializacion del viewcontrolles

---
+++ test_app1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test_app1
+//
+//  Created by Aldo Ulises on 27/09/14.
+//  Copyright (c) 2014 Aldo Ulises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
